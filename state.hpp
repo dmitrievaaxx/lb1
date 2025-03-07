d2e29150f22abe5328255b442d64a00bea7becc9
Darya Dmitrieva <144445820+dmitrievaaxx@users.noreply.github.com>
2025-03-07 22:06:43 +0300

    Update state.hpp

--- state.hpp
+++ state.hpp
@@ -5,10 +5,10 @@
 #include <glm/gtc/quaternion.hpp>
 
 struct State {
-    glm::dvec3 position; // координаты центра масс тела в трехмерном пространстве
-    glm::dvec3 momentum; // импульс (p = m * v)
-    glm::dquat quaternion; // кватернион
-    glm::dvec3 angular_momentum; // угловой момент тела (L = I * w, где I - тензор энерции, w - угловая скорость)
+    glm::dvec3 position; // Координаты центра масс тела в трехмерном пространстве
+    glm::dvec3 momentum; // Импульс (p = m * v)
+    glm::dquat quaternion; // Кватернион
+    glm::dvec3 angular_momentum; // Угловой момент тела (L = I * w, где I - тензор энерции, w - угловая скорость)
 
     State operator*(double scalar) const;
     State operator+(const State& other) const;
@@ -17,12 +17,14 @@ struct State {
 struct Constants {
     double mass;
     glm::dmat3 inertia_tensor;
+    double cube_size;
 };
 
 struct ExternalForces {
     glm::dvec3 gravity;
-    glm::dvec3 buoyancy; // сила Архимеда
-    glm::dvec3 drag; // сила сопротивления среды
+    glm::dvec3 buoyancy; // Сила Архимеда
+    glm::dvec3 drag; // Сила сопротивления среды
+    double rho; // Плотность жидкости для вычисления силы Архимеда
 };
 
 #endif
