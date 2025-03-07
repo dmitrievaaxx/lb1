4eae21d7346e8211002bb0e7f703f38a6dac096f
Darya Dmitrieva <144445820+dmitrievaaxx@users.noreply.github.com>
2025-03-07 22:07:16 +0300

    Update simulation.hpp

--- simulation.hpp
+++ simulation.hpp
@@ -2,9 +2,12 @@
 #define SIMULATION_HPP
 
 #include "state.hpp"
+#include "iostream"
 
 State compute_derivatives(const State& state, const Constants& constants, const ExternalForces& forces);
 
 void runge_kutta_4(State& state, const Constants& constants, const ExternalForces& forces, double dt);
+
+void print_energy(const State &state, const Constants &constants);
 
 #endif
